0ec644463135d32a8e611d971879b5d5fd69f6a6
tayeful912 <kazitayefulislam12@gmail.com>
2022-03-23 13:25:36 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css'
+
+const Header = () => {
+    return (
+
+        <nav className='header'>
+            <img src={logo} alt="" />
+        </nav>
+    );
+};
+
+export default Header;
