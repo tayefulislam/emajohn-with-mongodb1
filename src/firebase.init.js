98fd8b155f6b95180109a6ab47965e421bdadd13
tayeful912 <kazitayefulislam12@gmail.com>
2022-04-11 18:18:10 +0600

    ema john simple with auth

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,7 +7,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBeSdOyz5uw7IWRhI3WghYN1COjytvSq2I",
+    authDomain: "ema-jhon-simple1-4f2ca.firebaseapp.com",
+    projectId: "ema-jhon-simple1-4f2ca",
+    storageBucket: "ema-jhon-simple1-4f2ca.appspot.com",
+    messagingSenderId: "1079827941687",
+    appId: "1:1079827941687:web:e9eeeebef50b4db6b572b2"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
